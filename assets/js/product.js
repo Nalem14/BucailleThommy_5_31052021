4801f7128f3e3f12a1900549909114f76a279766
Thommy Bucaille <nalem.14360@gmail.com>
2021-06-08 15:13:39 +0200

    comment

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", async (event) => {
     const selectQuantity = document.getElementById('quantity');
 
+    // Show quantity select
     for(var i = 0; i < 10; i++) {
         var opt = document.createElement('option');
         opt.value = i+1;
@@ -8,15 +9,18 @@ window.addEventListener("DOMContentLoaded", async (event) => {
         selectQuantity.appendChild(opt);
     } 
 
+    // Get URL params
     const url = new URL(window.location.href);
     const id = url.searchParams.get("id");
     const category = url.searchParams.get("category");
 
+    // Get and show product from URL
     const result = await GetProduct(category, id);
     ShowProduct(result);
 });
 
 async function GetProduct(_category, _id) {
+
     return fetch('http://localhost:3000/api/' + _category + '/' + _id)
     .then(response => response.json())
     .then(datas => {
@@ -30,11 +34,15 @@ async function GetProduct(_category, _id) {
 }
 
 function ShowProduct(_result) {
+    // Set datas in view
+
     document.getElementById("product-name").innerHTML = _result.name;
     document.getElementById("product-img").src = _result.imageUrl;
     document.getElementById("product-description").innerHTML = _result.description;
     document.getElementById("product-price").innerHTML = _result.price + "€";
 
+    // Check for specifics options in select and get the correct array
+    // to show it
     if("colors" in _result) {
         ShowOptions(_result.colors);
         document.getElementById("options-name").innerHTML = "Couleur";
@@ -51,10 +59,11 @@ function ShowProduct(_result) {
     }
 }
 
-function ShowOptions(options) {
+function ShowOptions(_options) {
     const selectOption = document.getElementById("options");
 
-    options.forEach(element => {
+    // For each options, add it to the options select
+    _options.forEach(element => {
         console.log(element);
         var opt = document.createElement('option');
         opt.value = element;
